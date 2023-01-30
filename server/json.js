3971cfccfa7308eba3a2d7802c82a57089afcbda
MeShootIn <dmitriimishutin@gmail.com>
2023-01-30 18:02:57 +0500

    Add Editorconfig configuration

--- server/json.js
+++ server/json.js
@@ -1,74 +1,67 @@
-var __assign =
-  (this && this.__assign) ||
-  function () {
-    __assign =
-      Object.assign ||
-      function (t) {
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
         for (var s, i = 1, n = arguments.length; i < n; i++) {
-          s = arguments[i];
-          for (var p in s)
-            if (Object.prototype.hasOwnProperty.call(s, p)) t[p] = s[p];
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
         }
         return t;
-      };
+    };
     return __assign.apply(this, arguments);
-  };
+};
 /**
- * Асинхронный GET-запрос на сервер через fetch.
+ * Асинхронный запрос на сервер через fetch.
  * @param {string} url адрес.
  * @param {number} timeout таймаут в миллисекундах.
  * @returns {Promise<any>} промис, резолвящийся в JSON-объект.
  */
 function request(url, timeout, params) {
-  var abortController = new AbortController();
-  var timer = setTimeout(function () {
-    abortController.abort();
-  }, timeout);
-  return fetch(url, __assign({ signal: abortController.signal }, params))
-    .then(function (response) {
-      if (response.ok) {
-        return response.json();
-      }
-      throw response;
-    })
-    ['catch'](function (err) {
-      throw err;
-    })
-    ['finally'](function () {
-      clearTimeout(timer);
+    var abortController = new AbortController();
+    var timer = setTimeout(function () {
+        abortController.abort();
+    }, timeout);
+    return fetch(url, __assign({ signal: abortController.signal }, params))
+        .then(function (response) {
+        if (response.ok) {
+            return response;
+        }
+        throw response;
+    })["catch"](function (err) {
+        throw err;
+    })["finally"](function () {
+        clearTimeout(timer);
     });
 }
 // DEBUG
 function random(min, max) {
-  var rand = min + Math.random() * (max + 1 - min);
-  return Math.floor(rand);
+    var rand = min + Math.random() * (max + 1 - min);
+    return Math.floor(rand);
 }
 // DEBUG
 function fakeText(length) {
-  var result = '';
-  var characters =
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  var charactersLength = characters.length;
-  var counter = 0;
-  while (counter < length) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    counter += 1;
-  }
-  return result;
+    var result = '';
+    var counter = 0;
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    while (counter < length) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        counter += 1;
+    }
+    return result;
 }
 // TODO https://github.com/typicode/json-server#paginate
 var timeout = 3000;
 var req = {
-  url: 'http://localhost:3000/files?_limit=123',
-  // url: 'http://localhost:3000/files/2', // DELETE, PUT
-  params: {
-    method: 'GET',
-  },
+    url: 'http://localhost:3000/files?_limit=123',
+    // url: 'http://localhost:3000/files/2', // DELETE, PUT
+    params: {
+        method: 'GET'
+    }
 };
 request(req.url, timeout, req.params)
-  .then(function (response) {
+    .then(function (response) { return response.json(); })
+    .then(function (response) {
     console.log(response);
-  })
-  ['catch'](function (e) {
+})["catch"](function (e) {
     console.error('Fetch error:', e);
-  });
+});
--- server/json.ts
+++ server/json.ts
@@ -1,11 +1,15 @@
 /**
- * Асинхронный GET-запрос на сервер через fetch.
+ * Асинхронный запрос на сервер через fetch.
  * @param {string} url адрес.
  * @param {number} timeout таймаут в миллисекундах.
  * @returns {Promise<any>} промис, резолвящийся в JSON-объект.
  */
-function request(url, timeout, params) {
-  const abortController = new AbortController();
+function request(
+  url: string,
+  timeout: number,
+  params: object
+): Promise<Response> {
+  const abortController: AbortController = new AbortController();
   const timer = setTimeout(() => {
     abortController.abort();
   }, timeout);
@@ -14,9 +18,9 @@ function request(url, timeout, params) {
     signal: abortController.signal,
     ...params,
   })
-    .then((response) => {
+    .then((response: Response) => {
       if (response.ok) {
-        return response.json();
+        return response;
       }
 
       throw response;
@@ -30,18 +34,20 @@ function request(url, timeout, params) {
 }
 
 // DEBUG
-function random(min, max) {
-  let rand = min + Math.random() * (max + 1 - min);
+function random(min: number, max: number): number {
+  let rand: number = min + Math.random() * (max + 1 - min);
+
   return Math.floor(rand);
 }
 
 // DEBUG
-function fakeText(length) {
-  let result = '';
-  const characters =
+function fakeText(length: number): string {
+  let result: string = '';
+  let counter: number = 0;
+  const characters: string =
     'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  const charactersLength = characters.length;
-  let counter = 0;
+  const charactersLength: number = characters.length;
+
   while (counter < length) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
     counter += 1;
@@ -52,7 +58,7 @@ function fakeText(length) {
 
 // TODO https://github.com/typicode/json-server#paginate
 
-const timeout = 3000;
+const timeout: number = 3000;
 const req = {
   url: 'http://localhost:3000/files?_limit=123', // GET, POST
   // url: 'http://localhost:3000/files/2', // DELETE, PUT
@@ -84,7 +90,8 @@ const req = {
 };
 
 request(req.url, timeout, req.params)
-  .then((response) => {
+  .then((response: Response) => response.json())
+  .then((response: object) => {
     console.log(response);
   })
   .catch((e) => {
